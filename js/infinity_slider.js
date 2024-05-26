edd92c7355864eb3a6e63ccdbbb60b396583d759
LueraOchoaJoseLuis <jllueraochoa@gmail.com>
2024-05-26 14:57:25 -0500

    Se añadio un nuevo js para el slider infinito

---
+++ js/infinity_slider.js
@@ -0,0 +1,14 @@
+const DURATIONS = ["20s", "26s", "30s"]
+
+const duplicate = (index) => {
+  const clone = scroller.cloneNode(true)
+  clone.id = `id:${index}`
+  const sec = clone.querySelector('sec')
+  sec.style.animationDuration = DURATIONS[index];
+  sec.style.animationName = (index%2===0) ? 'scroll_right' : 'scroll_left'
+  document.body.appendChild(clone);
+}
+
+duplicate(0)
+duplicate(1)
+duplicate(2)
