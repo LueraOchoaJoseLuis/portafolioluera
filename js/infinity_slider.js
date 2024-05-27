0b086703568fa6da0c433016ae4f406e6deda310
LueraOchoaJoseLuis <jllueraochoa@gmail.com>
2024-05-26 22:04:50 -0500

    Arregle el slider infinito

--- js/infinity_slider.js
+++ js/infinity_slider.js
@@ -3,7 +3,7 @@ const DURATIONS = ["20s", "26s", "30s"]
 const duplicate = (index) => {
   const clone = scroller.cloneNode(true)
   clone.id = `id:${index}`
-  const sec = clone.querySelector('sec')
+  const sec = clone.querySelector('.sec')
   sec.style.animationDuration = DURATIONS[index];
   sec.style.animationName = (index%2===0) ? 'scroll_right' : 'scroll_left'
   document.body.appendChild(clone);
