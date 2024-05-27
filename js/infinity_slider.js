9b2b016aa6dd3b11fecf17309d570aad7a575ea7
LueraOchoaJoseLuis <jllueraochoa@gmail.com>
2024-05-26 23:10:56 -0500

    -Mejoras en el codigo

--- js/infinity_slider.js
+++ js/infinity_slider.js
@@ -1,14 +1,15 @@
-const DURATIONS = ["20s", "26s", "30s"]
+// const DURATIONS = ["20s", "26s", "30s"];
 
-const duplicate = (index) => {
-  const clone = scroller.cloneNode(true)
-  clone.id = `id:${index}`
-  const sec = clone.querySelector('.sec')
-  sec.style.animationDuration = DURATIONS[index];
-  sec.style.animationName = (index%2===0) ? 'scroll_right' : 'scroll_left'
-  document.body.appendChild(clone);
-}
+// const duplicate = (index) => {
+//   const scroller = document.getElementById('scroller');
+//   const clone = scroller.cloneNode(true);
+//   clone.id = `id:${index}`;
+//   const section = clone.querySelector('.infi'); // Corregido para seleccionar por clase en lugar de id
+//   section.style.animationDuration = DURATIONS[index];
+//   section.style.animationName = (index % 2 === 0) ? 'scroll_right' : 'scroll_left';
+//   document.body.appendChild(clone);
+// };
 
-duplicate(0)
-duplicate(1)
-duplicate(2)
+// duplicate(0);
+// duplicate(1);
+// duplicate(2);
